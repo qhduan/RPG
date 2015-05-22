c328750fe138dd47d2550699eddd5a422179e234
qhduan <mail@qhduan.com>
2015-05-22 21:14:02 +0800

    fix hero load bug

--- public/game/hero.js
+++ public/game/hero.js
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       if (img.complete) {
         ImageComplete();
       } else {
-        image.onload = ImageComplete;
+        img.onload = ImageComplete;
       }
     }
 
